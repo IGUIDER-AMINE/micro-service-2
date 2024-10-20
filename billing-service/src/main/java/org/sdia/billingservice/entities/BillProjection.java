b6f310f23384952bbd31c3ca6edab497924a456a
IGUIDER AMINE <amineiguider2017@gmail.com>
2024-10-20 23:25:41 +0200

    finish

--- billing-service/src/main/java/org/sdia/billingservice/entities/BillProjection.java
+++ billing-service/src/main/java/org/sdia/billingservice/entities/BillProjection.java
@@ -1,2 +1,13 @@
-package org.sdia.billingservice.entities;public interface BillProjection {
+package org.sdia.billingservice.entities;
+
+import org.springframework.data.rest.core.config.Projection;
+
+import java.util.Date;
+
+//http://localhost:8888/BILLING-SERVICE/bills/search/byCustomerId?customerId=1&projection=fullBill
+@Projection(name = "fullBill", types = Bill.class)
+public interface BillProjection {
+    Long getId();
+    Date getBillDate();
+    Long getCustomerId();
 }
